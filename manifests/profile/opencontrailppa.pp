bf2efbe938271286595ea23a59502141438776c4
Johannes Grassler <j.grassler@syseleven.de>
2014-10-16 10:42:03 +0200

    Parametrized package repository (defaults to our PPA repository for contrail-1.06).

--- manifests/profile/opencontrailppa.pp
+++ manifests/profile/opencontrailppa.pp
@@ -1,22 +1,10 @@
-class contrail::profile::opencontrailppa() {
+class contrail::profile::opencontrailppa(
+  $key = '24911626',
+  $source = 'ppa:syseleven-platform/contrail-1.06',
+  ) {
   apt::key { 'contrail.key':
-    key        => '6839FE77',
+    key        => $key,
   } 
-
-  # FIXME use better classes
-  exec {'echo | add-apt-repository ppa:opencontrail/ppa':
-    path     => '/usr/bin',
-    provider => 'shell',
-    unless   => '[ -f /etc/apt/sources.list.d/opencontrail-ppa-precise.list ]',
-  } ->
-
-  # TODO apt::source
-  file {'/etc/apt/sources.list.d/opencontrail-ppa-precise.list':
-    ensure  => file,
-    content => 'deb http://ppa.launchpad.net/opencontrail/snapshots/ubuntu precise main
-deb http://ppa.launchpad.net/opencontrail/ppa/ubuntu precise main
-',
-  } ~>
 
   exec {'aptitude update':
     path        => '/usr/bin',
@@ -24,12 +12,13 @@ deb http://ppa.launchpad.net/opencontrail/ppa/ubuntu precise main
     require     => Apt::Key['contrail.key'],
   }
 
+  apt::ppa { $source : }
+
   # FIXME snapshot of 1.06 .debs because opencontrail repo sometimes misses them
-  apt::source { 'contrail_repo_sys11_snapshot':
-    location    => 'https://files.syseleven.de/~sandres/contrail_deb ',
-    release     => '',
-    repos       => 'binary/',
-    include_src => false,
-  }
+#  apt::source { 'contrail_repo_sys11_snapshot':
+#    location    => 'https://files.syseleven.de/~sandres/contrail_deb',
+#    release     => '',
+#    repos       => 'binary/',
+#    include_src => false,
 }
 
