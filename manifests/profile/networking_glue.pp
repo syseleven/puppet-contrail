a7f61ed3110ffad4a511d7b330ece93ce00dfb94
Stefan Andres <s.andres@syseleven.de>
2014-10-20 14:33:36 +0200

    use ifup/down for networking restart in glue

--- manifests/profile/networking_glue.pp
+++ manifests/profile/networking_glue.pp
@@ -1,16 +1,16 @@
 class contrail::profile::networking_glue() {
   file {'/usr/local/bin/configure_network_interfaces':
-    ensure => file,
-    mode   => '555',
-    source => "puppet:///modules/$module_name/configure_network_interfaces",
+    ensure  => file,
+    mode    => '0555',
+    content => template("$module_name/configure_network_interfaces.erb"),
   }
 
   exec {'configure_network_interfaces':
-    command => '/usr/local/bin/configure_network_interfaces',
-    path    => '/usr/bin/:/bin/:/sbin/:/usr/sbin/',
-    unless  => '[ -f /etc/network/interfaces.d/vhost0.cfg ]',
+    command  => '/usr/local/bin/configure_network_interfaces',
+    path     => '/usr/bin/:/bin/:/sbin/:/usr/sbin/',
+    unless   => '[ -f /etc/network/interfaces.d/vhost0.cfg ]',
     provider => shell,
-    require => [File['/usr/local/bin/configure_network_interfaces'], Kmod::Load['vrouter']],
+    require  => [File['/usr/local/bin/configure_network_interfaces'], Kmod::Load['vrouter']],
   }
 }
 
