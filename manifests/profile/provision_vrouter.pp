d2c42fc179f2e4e7095c738b5a8373c0ca19fce7
Stefan Andres <s.andres@syseleven.de>
2014-10-20 13:55:18 +0200

    add sanity check if fact is available before exporting invalid command

--- manifests/profile/provision_vrouter.pp
+++ manifests/profile/provision_vrouter.pp
@@ -5,10 +5,12 @@ class contrail::profile::provision_vrouter(
   $action = false,
 ) {
   if $action == 'export' {
-    @@exec {"provision_vrouter $::hostname":
-      command => "/usr/sbin/contrail-provision-vrouter --host_name $::hostname --host_ip $::ipaddress_vhost0 --admin_user $keystone_admin_user --admin_password $keystone_admin_password --admin_tenant_name $keystone_admin_tenant_name --oper add",
-      tag  => 'provision_vrouter',
-      # FIXME implement unless parameter
+    if $::ipaddress_vhost0 {
+      @@exec {"provision_vrouter $::hostname":
+        command => "/usr/sbin/contrail-provision-vrouter --host_name $::hostname --host_ip $::ipaddress_vhost0 --admin_user $keystone_admin_user --admin_password $keystone_admin_password --admin_tenant_name $keystone_admin_tenant_name --oper add",
+        tag     => 'provision_vrouter',
+        # FIXME implement unless parameter
+      }
     }
   } elsif $action == 'import' {
     Exec <<| tag == "provision_vrouter" |>>
