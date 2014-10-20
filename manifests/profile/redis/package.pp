7099e0461b87abfc3f20a47a717bd8a3cfd4e4c1
Stefan Andres <s.andres@syseleven.de>
2014-10-20 13:44:08 +0200

    use ubuntu redis for trusty

--- manifests/profile/redis/package.pp
+++ manifests/profile/redis/package.pp
@@ -1,25 +1,28 @@
 class contrail::profile::redis::package() {
-  apt::key { 'redis.key':
-    key        => '5862E31D',
-  } 
+  if versioncmp($::operatingsystemrelease, '14') < 0 {
+    # ubuntu-12 does not have a recent enough redis-server
+    # needing at least 2.6.8
+    apt::key { 'redis.key':
+      key        => '5862E31D',
+    }
 
-  # FIXME use better classes
-  # need recent redis version, at least 2.6.8
-  exec {'echo | add-apt-repository  ppa:rwky/redis':
-    path     => '/usr/bin',
-    provider => 'shell',
-    unless   => '[ -f /etc/apt/sources.list.d/rwky-redis-precise.list ]',
-  } ~>
+    # FIXME use better classes
+    exec {'echo | add-apt-repository  ppa:rwky/redis':
+      path     => '/usr/bin',
+      provider => 'shell',
+      unless   => '[ -f /etc/apt/sources.list.d/rwky-redis-precise.list ]',
+      before   => Package['redis-server'],
+    } ~>
 
-  exec {'aptitude update redis':
-    command     => 'aptitude update',
-    path        => '/usr/bin',
-    refreshonly => true,
-    require     => Apt::Key['redis.key'],
-  } 
+    exec {'aptitude update redis':
+      command     => 'aptitude update',
+      path        => '/usr/bin',
+      refreshonly => true,
+      require     => Apt::Key['redis.key'],
+    }
+  }
 
   package {'redis-server':
     ensure  => latest,
-    require => Exec['echo | add-apt-repository  ppa:rwky/redis'],
   }
 }
