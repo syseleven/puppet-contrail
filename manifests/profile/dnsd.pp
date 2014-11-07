f584a0085271d70ce5873c9321741858e0284d6d
Maik Zumstrull <m.zumstrull@syseleven.de>
2014-11-07 16:36:01 +0100

    Only install contrail-dns on Contrail >= 1.10

--- manifests/profile/dnsd.pp
+++ manifests/profile/dnsd.pp
@@ -4,22 +4,28 @@ class contrail::profile::dnsd (
   $ifmap_password = hiera('contrail::dnsd::ifmap_password'),
 ) {
 
-  package { 'contrail-dns':
-    ensure => installed,
-  }
+  $contrail_version = hiera('contrail::version', '1.06')
 
-  file { '/etc/contrail/dns/dns.conf':
-    ensure  => file,
-    mode    => '444',
-    content => template("$module_name/contrail/dns.conf.erb"),
-    require => Package['contrail-dns'],
-    notify  => Service['contrail-dns'],
-  }
+  if versioncmp($contrail_version, '1.10') > 0 {
 
-  service { 'contrail-dns':
-    ensure  => running,
-    enable  => true,
-    require => Package['contrail-dns'],
+    package { 'contrail-dns':
+      ensure => installed,
+    }
+
+    file { '/etc/contrail/dns/dns.conf':
+      ensure  => file,
+      mode    => '444',
+      content => template("$module_name/contrail/dns.conf.erb"),
+      require => Package['contrail-dns'],
+      notify  => Service['contrail-dns'],
+    }
+
+    service { 'contrail-dns':
+      ensure  => running,
+      enable  => true,
+      require => Package['contrail-dns'],
+    }
+
   }
 
 }
