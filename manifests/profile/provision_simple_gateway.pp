7544e0792fc60efd78cd30fa8915738cb0ec9e53
Stefan Andres <s.andres@syseleven.de>
2014-11-12 15:10:21 +0100

    use proper PYTHONPATH and simpler subnet name

--- manifests/profile/provision_simple_gateway.pp
+++ manifests/profile/provision_simple_gateway.pp
@@ -9,7 +9,7 @@ class contrail::profile::provision_simple_gateway(
       ensure      => present,
       tenant_name => 'admin',
     }
-    neutron_subnet { 'private_subnet':
+    neutron_subnet { 'private':
       ensure       => present,
       cidr         => '10.0.0.0/24',
       network_name => 'private',
@@ -18,9 +18,10 @@ class contrail::profile::provision_simple_gateway(
   # network device must be create on compute node
   } elsif $action == 'create_vgw_device' {
     exec {'provision simple gateway':
-      command => 'python /usr/share/contrail-utils/provision_vgw_interface.py --oper create --interface vgw1 --subnets 10.0.0.0/24 --vrf default-domain:admin:private:private_subnet--routes 0.0.0.0/0',
-      path    => '/bin:/usr/bin:/sbin:/usr/sbin',
-      unless  => 'ifconfig vgw1',
+      command     => 'python /usr/share/contrail-utils/provision_vgw_interface.py --oper create --interface vgw1 --subnets 10.0.0.0/24 --vrf default-domain:admin:private:private --routes 0.0.0.0/0',
+      environment => ["PYTHONPATH=/usr/lib/python2.7/dist-packages/nova_contrail_vif/gen_py/instance_service"],
+      path        => '/bin:/usr/bin:/sbin:/usr/sbin',
+      unless      => 'ifconfig vgw1',
     }
   }
 }
