8c2fa3bf117e8dfda0f1aa9f8eb328bf9186f3dd
Stefan Andres <s.andres@syseleven.de>
2015-02-12 13:57:46 +0100

    os-266 set stricter webui config file permissions

--- manifests/profile/webui.pp
+++ manifests/profile/webui.pp
@@ -4,28 +4,31 @@ class contrail::profile::webui(
   $keystone_admin_tenant_name = hiera('contrail::keystone_admin_tenant_name'),
 ) {
 
-package{'contrail-web-controller': }
+  package{'contrail-web-controller': }
 
-service{'contrail-webui-jobserver':
-  ensure => 'running',
-}
+  service{'contrail-webui-jobserver':
+    ensure => 'running',
+  }
 
-service{'contrail-webui-webserver':
-  ensure => 'running',
-}
+  service{'contrail-webui-webserver':
+    ensure => 'running',
+  }
 
   file{'/etc/contrail/config.global.js':
     content => template("${module_name}/contrail/config.global.js.erb"),
-    mode    => '0644',
+    owner   => 'contrail',
+    group   => 'contrail',
+    mode    => '0440',
     require => Package['contrail-web-controller'],
     notify  => [Service['contrail-webui-jobserver'], Service['contrail-webui-webserver']],
   }
 
   file{'/etc/contrail/userAuth.js':
     content => template("${module_name}/contrail/userAuth.js.erb"),
-    mode    => '0644',
+    owner   => 'contrail',
+    group   => 'contrail',
+    mode    => '0440',
     require => Package['contrail-web-controller'],
     notify  => [Service['contrail-webui-jobserver'], Service['contrail-webui-webserver']],
   }
-
 }
