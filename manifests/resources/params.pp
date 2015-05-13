252ae036deecaf27c150f89dadf9e836c6c79dda
Johannes Grassler <j.grassler@syseleven.de>
2015-05-13 14:07:22 +0200

    os-606 Fixed variable name for $cassandra_server_list.

--- manifests/resources/params.pp
+++ manifests/resources/params.pp
@@ -2,5 +2,5 @@
 class contrail::resources::params(
   $controllers = hiera_hash('sys11stack::controllers', false),
 ) {
-    $rabbit_hosts = join(suffix(controllers2array($controllers, 'hostnames'), ':9160'), ' ')
+  $cassandra_server_list = join(suffix(controllers2array($controllers, 'hostnames'), ':9160'), ' ')
 }
