45e9adca87fddb394b69285a681f4138748722a3
Stefan Andres <s.andres@syseleven.de>
2015-06-12 16:52:21 +0200

    os-606 fix contrail-analytics-api-tcp check really

--- manifests/profile/analytics/analytics_api/monitoring.pp
+++ manifests/profile/analytics/analytics_api/monitoring.pp
@@ -1,11 +1,12 @@
 class contrail::profile::analytics::analytics_api::monitoring (
   $monitoring = hiera('sys11stack::monitoring', false),
   $listen_ip = hiera('contrail::analytics::analytics_api::listen_ip', 'localhost'),
+  $listen_port = hiera('contrail::analytics::analytics_api::listen_port', '8081'),
 ) {
   case $monitoring {
     'sensu':  {
       sensu::check{'contrail-analytics-api-tcp':
-        command => "/usr/lib/nagios/plugins/check_tcp  -H $listen_ip -p 8081",
+        command => "/usr/lib/nagios/plugins/check_tcp  -H $listen_ip -p $listen_port",
       }
 
       sensu::check{'contrail-analytics-api-process':
