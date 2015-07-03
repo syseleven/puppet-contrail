4e831a8cbe661a00feb9fd63e2b316c875ed395b
Stefan Andres <s.andres@syseleven.de>
2015-07-03 10:58:29 +0200

    os-681 add check_schema_leader

--- manifests/profile/schema_transformer/monitoring.pp
+++ manifests/profile/schema_transformer/monitoring.pp
@@ -1,16 +1,25 @@
 class contrail::profile::schema_transformer::monitoring (
-  $monitoring       = hiera('sys11stack::monitoring', false),
+  $controllers = hiera_hash('sys11stack::controllers', false),
+  $monitoring = hiera('sys11stack::monitoring', false),
 ) {
   case $monitoring {
     'sensu':  {
-      # TODO implement check for active/backup
-      # in backup mode schema will not listen on 8087
-      #sensu::check{'contrail-schema-transformer-tcp':
-      #    command => '/usr/lib/nagios/plugins/check_tcp  -H localhost -p 8087',
-      #  }
-
       sensu::check{'contrail-schema-transformer-process':
         command => '/usr/lib/nagios/plugins/check_procs -C contrail-schema -c 1:1',
+      }
+
+      if $controllers {
+        $controller_ips = controllers2array($controllers, 'ips')
+        $controller_ips_real = join($controller_ips, ' ')
+
+        file {'/usr/lib/nagios/plugins/check_schema_transformer_leader':
+          ensure => file,
+          mode   => '0555',
+          source => "puppet:///modules/$module_name/monitoring/check_schema_transformer_leader",
+        }->
+        sensu::check{'schema_transformer_leader':
+          command => "/usr/lib/nagios/plugins/check_schema_transformer_leader $controller_ips_real",
+        }
       }
     }
     false:  { }
